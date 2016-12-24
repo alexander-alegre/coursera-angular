03cfc2302480cdfda49c0a20b2a70a2a241af9c9
= <alexander.alegre@outlook.com>
2016-12-24 16:29:09 -0700

    add logic to display message depending on how many words the user typed

--- module1/scripts/app.js
+++ module1/scripts/app.js
@@ -1,13 +1,30 @@
 (function() {
 	angular.module('EatCalc' , [])
 	.controller('EatCalcController', function($scope) {
-		$scope.eatCalculations = function() {
-			return 'test';
 
-			$scope.messageId = 'goodMessage';
+		$scope.countWords = function(words) {
+			var numWords = function() {
+				// calculate the comma separated values
+
+				
+				return $scope.wordCount;
+			};
+			// call function above with the values
+			numWords();
+			// show message depending on how many words the user typed
+			if ($scope.wordCount > 3) {
+				$scope.eatMessage = 'You eat too much!';
+				$scope.messageId = 'badMessage';
+			} else if ($scope.wordCount < 4) {
+				$scope.eatMessage = 'Enjoy!';
+				$scope.messageId = 'goodMessage';
+			} else {
+				$scope.eatMessage = '';
+				$scope.messageId = 'noMessage';
+			}
+			return $scope.eatMessage;
+			return $scope.messageId;
 		};
 	});
-
-
-
 })();
+
