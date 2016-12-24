897298a77ef062840378898eb231afc4d64d1423
= <alexander.alegre@outlook.com>
2016-12-24 12:22:36 -0700

    add styles and base for AngularJS

---
+++ module1/scripts/app.js
@@ -0,0 +1,13 @@
+(function() {
+	angular.module('EatCalc' , [])
+	.controller('EatCalcController', function($scope) {
+		$scope.eatCalculations = function() {
+			return 'test';
+
+			$scope.messageId = 'goodMessage';
+		};
+	});
+
+
+
+})();
